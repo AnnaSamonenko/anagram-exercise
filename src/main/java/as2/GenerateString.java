257d96dd9fbacfd1fed29377c130c9344ac4dd4d
AnnaSamonenko <KarlMarks31>
2018-08-30 18:06:48 +0300

    With map

---
+++ src/main/java/as2/GenerateString.java
@@ -0,0 +1,19 @@
+package as2;
+
+import java.security.SecureRandom;
+
+public class GenerateString {
+    private static final String AB = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static SecureRandom rnd = new SecureRandom();
+
+    private GenerateString() {
+    }
+
+    public static String randomString(int len) {
+        StringBuilder sb = new StringBuilder(len);
+        for (int i = 0; i < len; i++)
+            sb.append(AB.charAt(rnd.nextInt(AB.length())));
+        return sb.toString();
+    }
+
+}
--- src/main/java/as2/Main.java
+++ src/main/java/as2/Main.java
@@ -1,12 +1,16 @@
 package as2;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        String string1 = "abcdef";
-        String string2 = "aacdfg";
+        long startTime = System.nanoTime();
+        String string1 = GenerateString.randomString(100000);
+        String string2 = GenerateString.randomString(100000);
+//        String string1 = "abcdef";
+//        String string2 = "aacdfg";
 
         try {
             verifyOnAnagrams(string1, string2);
@@ -14,33 +18,47 @@ public class Main {
             System.out.println("Length of the strings need to be equal");
         }
 
+        System.out.println((System.nanoTime() - startTime) / 1000000000);
     }
 
-    public static List<Character> verifyOnAnagrams(String str1, String str2) throws IllegalLengthOfAnagramException {
+    public static void verifyOnAnagrams(String str1, String str2) throws IllegalLengthOfAnagramException {
         if (str1.length() != str2.length())
             throw new IllegalLengthOfAnagramException("Length of the strings is not equal!");
-        List<Character> listOfCharacters1 = new ArrayList<>();
-        List<Character> listOfCharacters2 = new ArrayList<>();
+        Map<Character, Integer> mapOfCharacters1 = new HashMap<>();
+        Map<Character, Integer> mapOfCharacters2 = new HashMap<>();
 
-        createCharList(listOfCharacters1, str1);
-        createCharList(listOfCharacters2, str2);
+        createCharMap(mapOfCharacters1, str1);
+        createCharMap(mapOfCharacters2, str2);
 
-        for (int i = 0; i < listOfCharacters2.size(); i++) {
-            listOfCharacters1.remove(listOfCharacters2.get(i));
+        Set<Character> setOfKeys = mapOfCharacters1.keySet();
+        for (Character ch : setOfKeys) {
+            Integer amountOfCharIn1 = mapOfCharacters1.get(ch);
+            Integer amountOfCharIn2 = mapOfCharacters2.get(ch);
+            if (amountOfCharIn1 != null && amountOfCharIn2 != null)
+                mapOfCharacters1.put(ch, amountOfCharIn1 - amountOfCharIn2);
         }
 
-        if (listOfCharacters1.isEmpty()) {
+        Map<Character, Integer> collect = mapOfCharacters1.entrySet().stream()
+                .filter(x -> x.getValue() > 0)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        if (collect.isEmpty())
             System.out.println("Strings are anagrams");
-        } else {
-            System.out.println(listOfCharacters1);
+        else {
+            System.out.println(collect);
         }
-
-        return listOfCharacters1;
     }
 
-    public static void createCharList(List<Character> list, String str) {
-        for (int i = 0; i < str.length(); i++)
-            list.add(str.charAt(i));
+    public static void createCharMap(Map<Character, Integer> map, String str) {
+        for (int i = 0; i < str.length(); i++) {
+            Integer value = map.get(str.charAt(i));
+            if (value == null) {
+                map.put(str.charAt(i), 1);
+            } else {
+                value++;
+                map.put(str.charAt(i), value);
+            }
+        }
     }
 
 }
