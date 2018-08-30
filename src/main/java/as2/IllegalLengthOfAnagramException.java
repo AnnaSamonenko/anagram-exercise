212a3da1b0cccda9d9e69e14c3fad2f75a2e445d
AnnaSamonenko <KarlMarks31>
2018-08-30 11:31:03 +0300

    Added anagram exercise

---
+++ src/main/java/as2/IllegalLengthOfAnagramException.java
@@ -0,0 +1,8 @@
+package as2;
+
+public class IllegalLengthOfAnagramException extends Exception {
+
+    public IllegalLengthOfAnagramException(String message) {
+        super(message);
+    }
+}
--- src/main/java/as2/Main.java
+++ src/main/java/as2/Main.java
@@ -1,14 +1,41 @@
 package as2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        String str1 = "abcdef";
-        String str2 = "aacdfg";
+        String string1 = "elvis";
+        String string2 = "lives";
 
-        char[] array1 = str1.toCharArray();
-        char[] array2 = str2.toCharArray();
+        try {
+            verifyOnAnagrams(string1, string2);
+        } catch (IllegalLengthOfAnagramException ex) {
+            System.out.println("Length of the strings need to be equal");
+        }
+    }
 
+    public static void verifyOnAnagrams(String str1, String str2) throws IllegalLengthOfAnagramException {
+        if (str1.length() != str2.length())
+            throw new IllegalLengthOfAnagramException("Length of the strings is not equal!");
+        List<Character> listOfCharacters1 = new ArrayList<>();
+        List<Character> listOfCharacters2 = new ArrayList<>();
+
+        createCharList(listOfCharacters1, str1);
+        createCharList(listOfCharacters2, str2);
+
+        listOfCharacters1.removeAll(listOfCharacters2);
+
+        if (listOfCharacters1.isEmpty())
+            System.out.println("Strings are anagrams");
+        else
+            System.out.println(listOfCharacters1);
+    }
+
+    public static void createCharList(List<Character> list, String str) {
+        for (int i = 0; i < str.length(); i++)
+            list.add(str.charAt(i));
     }
 
 }
