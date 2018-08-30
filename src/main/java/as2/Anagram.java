136f114ca7640b528769b42161dea918f21bc466
AnnaSamonenko <KarlMarks31>
2018-08-30 18:36:30 +0300

    Test class added

--- src/main/java/as2/Anagram.java
+++ src/main/java/as2/Anagram.java
@@ -3,23 +3,12 @@ package as2;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Main {
+public class Anagram {
 
-    public static void main(String[] args) {
-        long startTime = System.nanoTime();
-        String string1 = GenerateString.randomString(100000);
-        String string2 = GenerateString.randomString(100000);
-
-        try {
-            verifyOnAnagrams(string1, string2);
-        } catch (IllegalLengthOfAnagramException ex) {
-            System.out.println("Length of the strings need to be equal");
-        }
-
-        System.out.println((System.nanoTime() - startTime) / 1000000000);
+    private Anagram() {
     }
 
-    public static void verifyOnAnagrams(String str1, String str2) throws IllegalLengthOfAnagramException {
+    public static Map<Character, Integer> verifyOnAnagrams(String str1, String str2) throws IllegalLengthOfAnagramException {
         if (str1.length() != str2.length())
             throw new IllegalLengthOfAnagramException("Length of the strings is not equal!");
         Map<Character, Integer> mapOfCharacters1 = new HashMap<>();
@@ -32,22 +21,23 @@ public class Main {
         for (Character ch : setOfKeys) {
             Integer amountOfCharIn1 = mapOfCharacters1.get(ch);
             Integer amountOfCharIn2 = mapOfCharacters2.get(ch);
-            if (amountOfCharIn1 != null && amountOfCharIn2 != null)
+            if (amountOfCharIn2 != null)
                 mapOfCharacters1.put(ch, amountOfCharIn1 - amountOfCharIn2);
         }
 
-        Map<Character, Integer> collect = mapOfCharacters1.entrySet().stream()
+        Map<Character, Integer> result = mapOfCharacters1.entrySet().stream()
                 .filter(x -> x.getValue() > 0)
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
 
-        if (collect.isEmpty())
+        if (result.isEmpty())
             System.out.println("Strings are anagrams");
         else {
-            System.out.println(collect);
+            System.out.println(result);
         }
+        return result;
     }
 
-    public static void createCharMap(Map<Character, Integer> map, String str) {
+    private static void createCharMap(Map<Character, Integer> map, String str) {
         for (int i = 0; i < str.length(); i++) {
             Integer value = map.get(str.charAt(i));
             if (value == null) {
---
+++ src/test/java/as/TestAnagramClass.java
@@ -0,0 +1,43 @@
+package as;
+
+import as2.Anagram;
+import as2.IllegalLengthOfAnagramException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestAnagramClass {
+
+    //sunny
+    @Test
+    public void testVerifyAnagramMethodPositive() {
+
+        String string1 = "abcdef";
+        String string2 = "aacdfg";
+
+        Map<Character, Integer> expectedResult = new HashMap<>();
+        Map<Character, Integer> actualResult;
+        expectedResult.put('b', 1);
+        expectedResult.put('e', 1);
+
+        try {
+            actualResult = Anagram.verifyOnAnagrams(string1, string2);
+            Assert.assertEquals(expectedResult, actualResult);
+        } catch (IllegalLengthOfAnagramException ex) {
+            System.out.println("Length of the strings need to be equal");
+        }
+    }
+
+    //rainy
+    @Test(expected = IllegalLengthOfAnagramException.class)
+    public void testVerifyAnagramMethodNegative() throws IllegalLengthOfAnagramException {
+
+        String string1 = "abcdef1";
+        String string2 = "aacdfg";
+
+        Anagram.verifyOnAnagrams(string1, string2);
+    }
+
+}
